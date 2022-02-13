960305420f7fe3ba6916530024b08a19f0d4d270
Alexandros5880 <Alexpl_15@gmail.com>
2022-02-13 18:51:00 +0200

    Register User API Action OK.

--- FullStack_Task/Areas/Identity/Controllers/AccountMController.cs
+++ FullStack_Task/Areas/Identity/Controllers/AccountMController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using FullStack_Task.Areas.Identity.Models;
+using FullStack_Task.Areas.Identity.Models.ViewModels;
 using FullStack_Task.HorizontalClasses.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FullStack_Task.Areas.Identity.Controllers
@@ -26,6 +29,43 @@ namespace FullStack_Task.Areas.Identity.Controllers
             ViewData["Areas"] = await this._db.BuisnessAreas.GetAll();
             ViewData["Countries"] = await this._geography.GetCountries();
             return View();
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(ApplicationUserViewModel viewModel)
+        {
+            if (viewModel == null)
+                return BadRequest();
+
+            // Create ApplicationUser
+            ApplicationUser user = this._mapper.Map<ApplicationUser>(viewModel);
+            await this._db.Users.Add(user);
+            await this._db.Save();
+
+            // Buisness Areas
+            List<BusinessArea> bareas = new List<BusinessArea>();
+            foreach (int areaId in viewModel.BAreas)
+            {
+                var businseArea = await this._db.BuisnessAreas.GetEmpty(areaId);
+                if (businseArea.Users == null)
+                    businseArea.Users = new List<ApplicationUser>();
+                businseArea.Users.Add(user);
+            }
+            user.BusinessAreas = bareas;
+            await this._db.Save();
+
+            // Create Comment
+            Comment comment = this._mapper.Map<Comment>(viewModel);
+            await this._db.Comments.Add(comment);
+            comment.User = user;
+
+            // Create Address
+            Address address = this._mapper.Map<Address>(viewModel);
+            address.User = user;
+            await this._db.Address.Add(address);
+            await this._db.Save();
+
+            return Ok(user);
         }
 
     }
